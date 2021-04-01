44536fcdd7fe6047ffe43980e9c7478ee24cd95e
(대학원생 기계공학과(공과대학)) CLINT ALEX STEED <steed@unist.ac.kr>
2021-04-01 16:50:42 +0900

    Added tracker to look ahead and smooth.

--- Assets/FollowWayPoints.cs
+++ Assets/FollowWayPoints.cs
@@ -6,31 +6,49 @@ public class FollowWayPoints : MonoBehaviour
 {
     public Transform[] waypoints;
     private int waypointCounter = 0;
-    public float speed = 10, turnSpeed = 5, distanceThreshold = 5;
+    public float speed = 10, turnSpeed = 5, distanceThreshold = 5, trackerSpeed = 10, trackerThreshold = 8;
     private Transform CurrentWaypoint => waypoints[waypointCounter];
-    // Start is called before the first frame update
+
+    private GameObject tracker;
+    
+    //todo: need to cache transform and tracker transform
+
     void Start()
     {
-        
+        tracker = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        tracker.transform.position = transform.position;
+        DestroyImmediate(tracker.GetComponent<Collider>());
     }
 
-    // Update is called once per frame
-    void Update()
+    void TrackerUpdate(Vector3 toTracker)
     {
-        Vector3 towaypoint = CurrentWaypoint.position - transform.position;
+        if (toTracker.magnitude > trackerThreshold) return;
+
+        Vector3 towaypoint = CurrentWaypoint.position - tracker.transform.position;
         if (towaypoint.magnitude < distanceThreshold)
         {
             waypointCounter++;
             if (waypointCounter >= waypoints.Length) waypointCounter = 0;
         }
-        LookAt(towaypoint);
-        
-        transform.Translate(0,0,speed*Time.deltaTime);
+
+        tracker.transform.rotation = Quaternion.LookRotation(towaypoint);
+        tracker.transform.Translate(0, 0, (speed + trackerSpeed) * Time.deltaTime);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 toTracker = tracker.transform.position - transform.position;
+        TrackerUpdate(toTracker);
+        LookAt(toTracker);
+
+        transform.Translate(0, 0, speed * Time.deltaTime);
     }
 
     void LookAt(Vector3 lookTowards)
     {
         Quaternion lookRotation = Quaternion.LookRotation(lookTowards);
-        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, turnSpeed*Time.deltaTime);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, turnSpeed * Time.deltaTime);
     }
 }
